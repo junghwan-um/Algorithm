7b75286fa1309c7328235b4bde62e74a4e455b5b
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-01-03 05:30:52 +0900

    [unrated] Title: 옹알이 (2), Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    set<string> words;
+    words.insert("aya"); words.insert("ye");
+    words.insert("woo"); words.insert("ma");
+    for (const auto &e : babbling) {
+        string temp;
+        string dup;
+        for (int i = 0; i < e.length(); ++i) {
+            temp += e[i];
+            if (find(words.begin(), words.end(), temp) != words.end()) {
+                if (temp != dup) {
+                    dup = temp;
+                    temp = "";
+                }
+            }
+        }
+        if (temp == "") ++answer;
+    }
+    
+    return answer;
+}
