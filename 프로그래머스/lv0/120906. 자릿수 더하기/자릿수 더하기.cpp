a7d65d33651c260099b55406812d752de77bb3a1
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-01-04 02:50:04 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.79 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string s = to_string(n);
+    for (const auto &e : s) answer += e - '0';
+    return answer;
+}
