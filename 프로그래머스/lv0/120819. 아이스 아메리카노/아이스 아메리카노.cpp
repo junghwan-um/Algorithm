3935e9f2abf8356604d9959d3e10e20d743f25a0
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-01-04 02:26:36 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
+    return answer;
+}
