2435b934dddb1379f0e844410b73837013abf2a0
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-01-23 01:06:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for (int i = 0; i < n; ++i) {
+        if (i % 2 == 0) answer += "수";
+        else if (i % 2 == 1) answer += "박";
+    }
+    return answer;
+}
