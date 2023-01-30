e8954e1e2661586967bae76a26ac0436f0fcf3f9
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-01-30 11:44:40 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    string temp;
+    map<int, int> mp;
+    for (int i = 0; i < food.size(); ++i) {
+        if (food[i] % 2 == 1) food[i] -= 1;
+        if (food[i]) {
+            for (int j = 0; j < food[i] / 2; ++j) temp += to_string(i);
+        }
+    }
+    answer += temp;
+    answer += "0";
+    reverse(temp.begin(), temp.end());
+    answer += temp;
+    return answer;
+}
