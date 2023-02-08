1b7a0bac200681a17caa9e6fd1feb3303c0aeb39
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-02-09 05:37:06 +0900

    [level 2] Title: 주식가격, Time: 15.58 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    for (int i = 0; i < prices.size() - 1; ++i) {
+        int flag = 0;
+        for (int j = i + 1; j < prices.size(); ++j) {
+            if (prices[i] > prices[j]) {
+                flag = 1;
+                answer.push_back(j - i);
+                break;
+            }
+        }
+        if (!flag) answer.push_back(prices.size() - i - 1);
+    }
+    answer.push_back(0);
+    return answer;
+}
