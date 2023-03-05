0741e223b520b2db0edae99470d8ec59d87d9168
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-03-06 06:15:23 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+    
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] > i) answer = i + 1; 
+        else break;
+    }
+    return answer;
+}
