a016852853cf9f174401e05598e4e6fbf15dadef
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-03-06 10:07:32 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string) {
+    for (auto &e : my_string) e = tolower(e);
+    sort(my_string.begin(), my_string.end());
+    return my_string;
+}
