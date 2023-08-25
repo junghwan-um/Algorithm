f693e4f0fa0f8fb03239c3400b42488d3b613f29
Junghwan_Um <119525410+junghwan-um@users.noreply.github.com>
2023-08-25 15:04:40 +0900

    [unrated] Title: rny_string, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string rny_string) {
+    while (rny_string.find("m") != string::npos) rny_string.replace(rny_string.find("m"), 1, "rn");
+    return rny_string;
+}
